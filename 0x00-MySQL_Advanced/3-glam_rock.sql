a541de4c3a974bdfae4a20650116cc99eeb902da
victormwachi <mudakizevictor@gmail.com>
2023-07-14 00:38:22 +0300

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,2 @@
 -- lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (2022-formed) AS life_span FROM metal_bands ORDER BY life_span;
+SELECT band_name, (IFNULL(split,2022)-formed) AS life_span FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY life_span DESC;
