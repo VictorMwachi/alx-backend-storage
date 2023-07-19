97fe7d5e5a8cf38ccc551ac35f19249e2a06c118
Victor Mwachi <mudakizevictor@gmail.com>
2023-07-19 20:38:20 +0000

    parametize

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -23,3 +23,24 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self,
+            key: str,
+            fn: Callable = None
+            ) -> Union[bytes, str, float, int]:
+        """
+        take a key string argument and an optional Callable argument named fn.
+        This callable will be used to convert the data back
+        to the desired format.
+        """
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        """will automatically parametrize Cache.get to str"""
+        return self.get(key, lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """will automatically parametrize Cache.get to int"""
+        return self.get(key, lambda x: int(x))
