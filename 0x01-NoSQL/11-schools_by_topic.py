65366b119a3b8c071d5be597b16bbe38bf5a69a9
victormwachi <mudakizevictor@gmail.com>
2023-07-21 23:34:45 +0300

    11-schools_by_topic.py

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+11. Where can I learn Python?
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns school having specific topic"""
+    school = mongo_collection.find({"topics": topic})
+    return school
